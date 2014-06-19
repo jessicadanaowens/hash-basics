9f7fa93fa3d09318a522d50ed277c8853ced83a1
Jessica Owens <jessicadanaowens@gmail.com>
2014-06-19 13:55:39 -0600

    print favorite foods'

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,3 +1,7 @@
 require_relative "person"
 
 # Print Bjorn's favorite foods. It should read "Bjorn's favorite foods are sushi, hamburgers, and mexican food."
+
+p "Bjorn's favorite foods are #{BJORN_BORG["favorite_foods"].join(', ')}"
+
+
