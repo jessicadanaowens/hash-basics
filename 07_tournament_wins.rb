84bd238ffd97c7999bd2fb84ea16e7589458fb43
Jessica Owens <jessicadanaowens@gmail.com>
2014-06-19 14:21:26 -0600

    print first and last wins

--- 07_tournament_wins.rb
+++ 07_tournament_wins.rb
@@ -8,3 +8,10 @@ wins = {
     wimbledon: ["1976", "1977", "1978", "1979", "1980"]
   }
 }
+
+BJORN_BORG[:wins] = wins
+puts "BJORN's first win was #{BJORN_BORG[:wins][:tournament_wins][:wimbledon].first}"
+puts "BJORN's last win was #{BJORN_BORG[:wins][:tournament_wins][:wimbledon].last}"
+
+
+
